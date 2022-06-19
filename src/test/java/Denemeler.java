376a8097ada206a6ee0802d150444ad71e17425a
Fatih Turker <fthturker80@gmail.com>
2022-06-19 12:26:55 +0300

    19.06.2022

--- src/test/java/Denemeler.java
+++ src/test/java/Denemeler.java
@@ -11,58 +11,27 @@ import org.testng.Assert;
 
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import utulities.Driver;
 import utulities.TestBase;
 
 import java.util.List;
 
 
-public class Denemeler extends TestBase {
+public class Denemeler  {
 
     @Test
-    public void test01() throws InterruptedException {
-        //1. “http://zero.webappsecurity.com/” Adresine gidin
-        driver.get("http://zero.webappsecurity.com/");
+    public void test01() {
+        Driver.getDriver().get("https://www.amazon.com");
+        /*
+        Bugune kadar TestBase class'ina extends ederek kullandigimiz driver yerine
+        bundan sonra Driver class'indan kullanacagimiz getDriver static method'unu kullanacagiz
 
-        //2. Sign in butonuna basin
-        driver.findElement(By.id("signin_button")).click();
-        Thread.sleep(2000);
-        //3. Login kutusuna “username” yazin
-        WebElement loginKutusu= driver.findElement(By.xpath("//input[@id='user_login']"));
-        loginKutusu.sendKeys("username");
-        Thread.sleep(2000);
-        //4. Password kutusuna “password” yazin
-        WebElement passwordKutusu=driver.findElement(By.xpath("//input[@id='user_password']"));
-        passwordKutusu.sendKeys("password");
-        Thread.sleep(2000);
-        //5. Sign in tusuna basin
-        driver.findElement(By.xpath("//input[@name='submit']")).click();
-        driver.navigate().back();
-        Thread.sleep(3000);
-
-        //6. Online banking menusu icinde Pay Bills sayfasina gidin
-        driver.findElement(By.xpath("//strong[normalize-space()='Online Banking']")).click();
-        driver.findElement(By.xpath("//span[@id='pay_bills_link']")).click();
-        Thread.sleep(2000);
-        //7. “Purchase Foreign Currency” tusuna basin
-        driver.findElement(By.xpath("//a[normalize-space()='Purchase Foreign Currency']")).click();
-        Thread.sleep(2000);
-        //8. “Currency” drop down menusunden Eurozone’u secin
-        WebElement ddo=driver.findElement(By.xpath("//select[@id='pc_currency']"));
-        Select select= new Select(ddo);
-        select.selectByVisibleText("Eurozone (euro)");
-        Thread.sleep(2000);
-        //9. soft assert kullanarak "Eurozone (Euro)" secildigini test edin
-
-
-
-        //10. soft assert kullanarak DropDown listesinin su secenekleri oldugunu test edin
-        // "Select One", "Australia (dollar)", "Canada (dollar)","Switzerland (franc)",
-        // "China (yuan)","Denmark (krone)","Eurozone (euro)","Great Britain (pound)",
-        // "Hong Kong (dollar)","Japan (yen)","Mexico (peso)","Norway (krone)",
-        // "New Zealand (dollar)","Sweden (krona)","Singapore (dollar)","Thailand (baht)"
-
-
-
-
+        Driver.getDriver()    in
+        driver                out
+         */
+        Driver.closeDriver();
+        Driver.getDriver().get("https://www.bestbuy.com");
+        Driver.getDriver().get("https://www.facebook.com");
+        Driver.closeDriver();
     }
 }
